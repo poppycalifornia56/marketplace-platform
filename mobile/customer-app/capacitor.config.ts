82accbf26f167442c8f8f2366691bc487c566499
poppy california <johannnhandoko@gmail.com>
2025-07-01 11:57:33 +0200

    project structure done

---
+++ mobile/customer-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marketplace.customer',
+  appName: 'customer-app',
+  webDir: 'www'
+};
+
+export default config;
---
+++ mobile/vendor-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marketplace.vendor',
+  appName: 'vendor-app',
+  webDir: 'www'
+};
+
+export default config;
